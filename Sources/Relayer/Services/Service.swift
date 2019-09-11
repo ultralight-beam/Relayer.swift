9bc84573406065815873c91f3b3dfc410b0c6ca6
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-09-10 20:01:20 -0400

    feature/relayer-skeleton
    * dummy framework
    
    * todo
    
    * format
    
    * commented
    
    * fix

---
+++ Sources/Relayer/Services/Service.swift
@@ -0,0 +1,7 @@
+import Foundation
+import UB
+
+/// Services are responsible for handling messages received from an Ultralight Beam node.
+protocol Service: AnyObject {
+    func handle(message: UB.Message)
+}
--- Sources/Relayer/main.swift
+++ Sources/Relayer/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import Foundation
+import UB
+
+let handler = Handler()
+
+let node = UB.Node()
+node.delegate = handler
+node.add(transport: CoreBluetoothTransport())
+
+// @todo handle CLI input, repl
+
+RunLoop.current.run()
