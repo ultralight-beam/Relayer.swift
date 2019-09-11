87281f7e9001fb90b019a87da28b0025c0066d05
decanus <dean.eigenmann@icloud.com>
2019-09-10 21:43:29 -0400

    some ugly stuff

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ultralight-beam/UB.swift.git", .branch("master")),
-        // .package(path: "../UB.swift/"),
+        // .package(url: "https://github.com/ultralight-beam/UB.swift.git", .branch("master")),
+        .package(path: "../UB.swift/"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/Relayer/main.swift
+++ Sources/Relayer/main.swift
@@ -4,9 +4,30 @@ import UB
 let handler = Handler()
 
 let node = UB.Node()
+let t = CoreBluetoothTransport()
 node.delegate = handler
-node.add(transport: CoreBluetoothTransport())
+node.add(transport: t)
 
-// @todo handle CLI input, repl
+func input() -> String {
+    
+    let keyboard = FileHandle.standardInput
+    
+    let inputData = keyboard.availableData
+    
+    let strData = String(data: inputData, encoding: .utf8)
+    
+    return strData!
+    
+}
+
+func read() -> String {
+    let c = CharacterSet.whitespacesAndNewlines
+    return input().trimmingCharacters(in: c)
+}
+//
+//while true {
+//    print(t.peers)
+//    print(read())
+//}
 
 RunLoop.current.run()
