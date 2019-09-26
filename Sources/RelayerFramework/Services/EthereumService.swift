60cc8d471d0f37d3463cd40e634280107575e40c
Eric Tu <tu.eric@hotmail.com>
2019-09-26 14:18:18 -0400

    async handling

--- Sources/RelayerFramework/Services/EthereumService.swift
+++ Sources/RelayerFramework/Services/EthereumService.swift
@@ -1,6 +1,5 @@
 import Foundation
 import UB
-// import web3
 
 public class EthereumService: Service {
     let url: URL
@@ -10,10 +9,42 @@ public class EthereumService: Service {
         self.url = url
     }
     
+    internal func httpRequest (request: URLRequest, completion: @escaping (Result<Data, Error>) -> Void) {
+        _ = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                completion(.failure(error!))
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
+    
+    internal func getBalance(address: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let payload: [String: Any] = ["jsonrpc": "2.0",
+                                      "method": "eth_getBalance",
+                                      "params":[address, "latest"],
+                                      "id": 1]
+        let jsonPayload = try! JSONSerialization.data(withJSONObject: payload)
+        request.httpBody = jsonPayload
+        
+        httpRequest(request: request){ result in
+            switch result {
+            case .success (let balance):
+                completion(.success(balance))
+            case .failure (let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
     // handle gets called when an Ethereum service is called.
     public func handle(message: Message, node: Node) {
         // The first btye of the message is the JSONRPC method
-        let methodID = message.message[0]
+        let methodID = String(data: message.message[0..<1], encoding: .utf8)!
+        print(methodID)
         
         let url = URL(string: "https://rinkeby.infura.io/f7a08ae0242843f1b1cf480454a6bba5")!
         var request = URLRequest(url: url)
@@ -26,18 +57,27 @@ public class EthereumService: Service {
         
         
         switch methodID {
-        case 0:
+        case "0":
             let address = String(data: message.message[1..<22], encoding: .utf8)!
-            payload["method"] = "eth_getBalance"
-            payload["params"] = [address, "latest"]
-            
-            jsonPayload = try! JSONSerialization.data(withJSONObject: payload)
-            request.httpBody = jsonPayload
-            
-        case 1:
+            getBalance(address: address){ result in
+                switch result{
+                case .success (let balance):
+                    let messageResponse = Message(
+                        proto: UBID(repeating: 1, count: 1),
+                        recipient: message.origin,
+                        from: message.recipient,
+                        origin: message.recipient,
+                        message: balance
+                    )
+                    node.send(messageResponse)
+                case .failure (let error):
+                    print(error)
+                }
+            }
+        case "1":
             print("eth_getTxCount not impled")
             return
-        case 2:
+        case "2":
             //eth.sendRawTx
             print("eth_sendRawTx not impled")
             return
@@ -46,21 +86,8 @@ public class EthereumService: Service {
             return
         }
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print(error?.localizedDescription ?? "No data")
-                return
-            }
-            let messageResponse = Message(
-                proto: UBID(repeating: 1, count: 1),
-                recipient: message.origin,
-                from: message.recipient,
-                origin: message.recipient,
-                message: data
-            )
-            node.send(messageResponse)
-        }
-        task.resume()
+        
+        print("22222")
     }
 }
 
---
+++ Tests/RelayerFrameworkTests/stubs/Transport.swift
@@ -0,0 +1,8 @@
+//
+//  Transport.swift
+//  Relayer
+//
+//  Created by Eric Tu on 9/25/19.
+//
+
+import Foundation
