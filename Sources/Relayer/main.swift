8aafd8820dccec31622821fcdf369a00ea7ea494
decanus <dean.eigenmann@icloud.com>
2019-09-30 22:28:16 +0200

    done

--- Sources/Relayer/main.swift
+++ Sources/Relayer/main.swift
@@ -9,40 +9,15 @@ let t = CoreBluetoothTransport()
 node.delegate = handler
 node.add(transport: t)
 
-func input() -> String {
-    
-    let keyboard = FileHandle.standardInput
-    
-    let inputData = keyboard.availableData
-    
-    let strData = String(data: inputData, encoding: .utf8)
-    
-    return strData!
-    
-}
-
-func read() -> String {
-    let c = CharacterSet.whitespacesAndNewlines
-    return input().trimmingCharacters(in: c)
-}
-
-while true {
-    let message = read()
-    if message == "" {
-        continue
-    }
-
-    guard let data = message.data(using: .utf8) else { continue }
+while let line = readLine() {
+    guard let message = line.data(using: .utf8) else { continue }
     node.send(
         Message( // bunch of dummy fields for now as we don't do identity
             proto: UBID(repeating: 1, count: 1),
             recipient: UBID(repeating: 1, count: 1),
             from: UBID(repeating: 1, count: 1),
             origin: UBID(repeating: 1, count: 1),
-            message: data
+            message: message
         )
     )
-
 }
-
-//RunLoop.current.run()
--- Sources/RelayerFramework/Services/PlainTextMessagingService.swift
+++ Sources/RelayerFramework/Services/PlainTextMessagingService.swift
@@ -1,10 +1,11 @@
 import Foundation
+import UB
 
 /// Basic service for receiving plain text messages from other nodes.
 public class PlainTextMessagingService: Service {
 
     /// :nodoc:
-    func handle(message: UB.Message) {
+    public func handle(message: UB.Message) {
         guard let data = String(data: message.message, encoding: .utf8) else { return }
 
         // @todo probably more info like from etc.
