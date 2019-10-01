68e346daf62fb8cd5e0121565c50f5a0274afbba
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-10-01 22:09:22 +0200

    rename/relayer-lowercase (#8)
    * rename
    
    * Rename Sources/Relayer/main.swift to Sources/relayer/main.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
     products: [
         .executable(
-            name: "Relayer",
-            targets: ["Relayer"]
+            name: "relayer",
+            targets: ["relayer"]
         ),
         .library(
             name: "RelayerFramework",
@@ -28,7 +28,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Relayer",
+            name: "relayer",
             dependencies: ["RelayerFramework"]
         ),
         .target(
