81180f9bb1a66ca35d47b33bfe28df1af27887bd
decanus <dean.eigenmann@icloud.com>
2019-10-05 02:43:35 +0200

    removes prefix

--- Sources/RelayerFramework/Services/Service.swift
+++ Sources/RelayerFramework/Services/Service.swift
@@ -2,7 +2,7 @@ import Foundation
 import UB
 
 /// An interface used to handle messages for a specific service.
-public protocol Service: UB.NodeDelegate {
+public protocol Service: NodeDelegate {
     /// The identifier of the service.
     var identifier: UBID { get }
 }
